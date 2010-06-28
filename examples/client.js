20966096cd1267e0c76fa12f00515467f7159300
Austin Chau <austin.chau@gmail.com>
2010-06-28 00:27:21 -0700

    rollback the collecting chunks code on 'data' event - this relies on 'end' or EOF being sent from other msgpack client which isn't part of spec.  So the actual call to the impl needs to be done on connect.

--- examples/client.js
+++ examples/client.js
@@ -3,15 +3,6 @@ var RpcClient = require('../msgpackrpc').RpcClient;
 
 var client = new RpcClient('localhost', 9999);
 
-var hugeStr = '';
-
-for (var i=0; i<100000; i++) {
-  hugeStr += 'a';
-}
-client.invoke('echo', [hugeStr], function(result, error) {
-  sys.puts(result.length);
-});
-
 client.invoke('add', [9, 3], function(result, error) {
   if (error) {
     sys.puts('[rpc error] ' + error);
